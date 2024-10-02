60eff238ae6e8196e4c380a8d140ad5a4561d642
Oskar Stone <77391088+ofs416@users.noreply.github.com>
2024-10-02 21:15:12 +0100

    Update cpu_functions.c
    added matrix addition (untested)

--- cpu_functions.c
+++ cpu_functions.c
@@ -6,3 +6,10 @@ void vectorAdditionCPU (float *a, float *b, float *c, int n) {
   }
 }
 
+void matrixAdditionCPU (float *a, float *b, float *c, int n, int m) {
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+      c[i][j] = a[i][j] + b[i][j];
+    }
+  }
+}
