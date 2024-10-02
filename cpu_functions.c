5ce0e386a826d3c38641c6e1d2992b64c7577f78
Oskar Stone <77391088+ofs416@users.noreply.github.com>
2024-10-02 22:00:29 +0100

    use of proper syntax

--- cpu_functions.c
+++ cpu_functions.c
@@ -9,7 +9,7 @@ void vectorAdditionCPU (float *a, float *b, float *c, int n) {
 void matrixAdditionCPU (float *A, float *B, float *C, int n) {
   for (int i = 0; i < n; i++){
     for (int j = 0; j < n; j++){
-      C[i][j] = A[i][j] + A[i][j];
+      C[i + n * j] = A[i + n * j] + B[i + n * j];
     }
   }
 }
@@ -17,11 +17,11 @@ void matrixAdditionCPU (float *A, float *B, float *C, int n) {
 void matrixMultiplicationCPU (float *A, float *B, float *C, int n, int m, int k) {
   for (int i = 0; i < n; i++){
     for (int j = 0; j < m; j++){
-      float sum = 0.0;
+      float sum = 0.0f;
       for (int l = 0; l < k; l++){
-        sum += A[i * k + l] * B[l * m + j]
+        sum += A[i * k + l] * B[l * m + j];
       }
-      C[i, j] = sum;
+      C[i * m + j] = sum;
     }
   }
 }
