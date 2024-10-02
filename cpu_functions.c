560dda6abf726b0f9fcb4212baa73d5bff4e0aab
Oskar Stone <77391088+ofs416@users.noreply.github.com>
2024-10-02 21:23:08 +0100

    Update cpu_functions.c
    matmul

--- cpu_functions.c
+++ cpu_functions.c
@@ -6,10 +6,22 @@ void vectorAdditionCPU (float *a, float *b, float *c, int n) {
   }
 }
 
-void matrixAdditionCPU (float *a, float *b, float *c, int n, int m) {
+void matrixAdditionCPU (float *A, float *B, float *C, int n) {
   for (int i = 0; i < n; i++){
     for (int j = 0; j < n; j++){
-      c[i][j] = a[i][j] + b[i][j];
+      C[i][j] = A[i][j] + A[i][j];
+    }
+  }
+}
+
+void matrixMultiplicationCPU (float *A, float *B, float *C, int n, int m, int k) {
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < m; j++){
+      float sum = 0.0;
+      for (int l = 0; l < k; l++){
+        sum += A[i * k + l] * B[l * m + j]
+      }
+      C[i, j] = sum;
     }
   }
 }
