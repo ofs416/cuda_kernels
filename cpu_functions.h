4eac477b89f55a8d9ac603cc5a4f59bb4e20712c
Oskar Stone <77391088+ofs416@users.noreply.github.com>
2024-10-05 00:40:22 +0100

    Update cpu_functions.h

--- cpu_functions.h
+++ cpu_functions.h
@@ -1,8 +1,8 @@
-#ifndef CPU_FUNCTIONS_H
-#define CPU_FUNCTIONS_H
+#ifndef CPU_FUNCTIONS_H_
+#define CPU_FUNCTIONS_H_
 
 void initMatrix(float *a, int row, int col);
 void matrixMultiplicationCPU(float *A, float *B, float *C, int n, int k, int m);
 double getTime();
 
-#endif // CPU_FUNCTIONS_H
+#endif // CPU_FUNCTIONS_H_
