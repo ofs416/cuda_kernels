a028cd824888e4c212e93cf9ea52001f6bec9eef
os416 <os416@cam.ac.uk>
2024-10-07 18:32:23 +0100

    Shared Memory Cache-Blocking

--- gpu_functions.cu
+++ gpu_functions.cu
@@ -1,3 +1,4 @@
+#include <cuda_runtime.h>
 #include "gpu_functions.h"
 
 #define BLOCK_SIZE 32
@@ -43,4 +44,39 @@ __global__ void gemm_gmc (float *A, float *B, float *C, int n, int k, int m) {
         }
         C[n * row + col] = sum;
     }
+}
+
+// Shared Memory CUDA kernel for matrix multiplication (N x K) @ (K x M)
+// Shared memory is has a size of O(KB) and bandwidth of >10,000GB/s
+// Global memory has a bandwidth of <1,000GB/s 
+// Each thread block is responsible for computing a sub-matrix of C
+// Each thread block loads a tile of A and B into shared memory
+__global__ void smem_gmc(float *A, float *B, float *C, int n, int k, int m) {
+    __shared__ float A_shared[BLOCK_SIZE * BLOCK_SIZE];
+    __shared__ float B_shared[BLOCK_SIZE * BLOCK_SIZE];
+
+    const uint blockRow = blockIdx.x;
+    const uint blockCol = blockIdx.y;
+    const uint threadCol = threadIdx.x % BLOCK_SIZE;
+    const uint threadRow = threadIdx.x / BLOCK_SIZE;
+
+    A += blockRow * BLOCK_SIZE * k;                  
+    B += blockCol * BLOCK_SIZE;                       
+    C += blockRow * BLOCK_SIZE * n + blockCol * BLOCK_SIZE; 
+
+    float tmp = 0.0f;
+    for (int blkIdx = 0; blkIdx < k; blkIdx += BLOCK_SIZE) {
+        A_shared[threadRow * BLOCK_SIZE + threadCol] = A[threadRow * k + threadCol];
+        B_shared[threadRow * BLOCK_SIZE + threadCol] = B[threadRow * n + threadCol];
+
+        __syncthreads();
+        A += BLOCK_SIZE;
+        B += BLOCK_SIZE * n;
+
+        for (int dotIdx = 0; dotIdx < BLOCK_SIZE; ++dotIdx) {
+            tmp += A_shared[threadRow * BLOCK_SIZE + dotIdx] * B_shared[dotIdx * BLOCK_SIZE + threadCol];
+        }
+        __syncthreads();
+    }
+    C[threadRow * n + threadCol] = tmp;
 }
--- gpu_functions.h
+++ gpu_functions.h
@@ -4,5 +4,6 @@
 __global__ void matrixAdditionGPU(float *A, float *B, float *C, int n);
 __global__ void matrixMultiplicationGPU(float *A, float *B, float *C, int n, int k, int m);
 __global__ void gemm_gmc (float *A, float *B, float *C, int n, int k, int m);
+__global__ void smem_gmc (float *A, float *B, float *C, int n, int k, int m);
 
 #endif // GPU_FUNCTIONS_H_
