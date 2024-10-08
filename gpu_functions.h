2de30ee2e84bbd7453d3c572867f5cdb1a43f050
ofs416 <oskarfstone@gmail.com>
2024-10-08 00:42:01 +0000

    refactoring

--- gpu_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef GPU_FUNCTIONS_H_
-#define GPU_FUNCTIONS_H_
-
-__global__ void matrixAdditionGPU(float *A, float *B, float *C, int n);
-__global__ void gemm (float *A, float *B, float *C, int n, int k, int m);
-__global__ void gemm_gmc (float *A, float *B, float *C, int n, int k, int m);
-__global__ void gemm_smem (float *A, float *B, float *C, int n, int k, int m);
-
-#endif // GPU_FUNCTIONS_H_
