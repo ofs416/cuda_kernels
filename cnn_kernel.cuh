9a9bcbc98f7bc5ab19e8d46be50455df8c760845
Oskar Stone <77391088+ofs416@users.noreply.github.com>
2024-10-12 19:11:19 +0100

    Update cnn_kernel.cuh

--- cnn_kernel.cuh
+++ cnn_kernel.cuh
@@ -1,6 +1,6 @@
-#ifndef CNN_FUNCTIONS_H_
+#ifndef CNN_KERNELS_H
 #define CNN_KERNELS_H
 
 __global__ void cnn (float *A, float *B, float *C, uint n, uint k, uint m);
 
-#endif // CNN_FUNCTIONS_H_
+#endif // CNN_KERNELS_H
